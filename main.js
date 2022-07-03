e066ac54ab075019d75e5dcdccaa53db9bb89fe1
Kanav Krishan <kanavk61@gmail.com>
2022-07-03 16:08:53 +0530

    Completed halfway through

--- main.js
+++ main.js
@@ -1,1 +1,118 @@
-console.log("changes");
+let inputArr = process.argv.slice(2);
+let fs = require("fs");
+let path = require("path");
+/* The above line is taking the user supplied arguements and 
+putting them into an array 
+
+Uses slice() method to create a new array after the 1st index incluseive 2. */
+
+console.log(inputArr);
+let command = inputArr[0]; //We will take the first arguement as the command which we want to perform
+//Will use switch statement to select which function to execute
+let types = {
+  media: ["mp4", "mkv", "jpg", "jpeg"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "ods",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+  ],
+  app: ["exe", "dmg", "pkg", "deb"],
+};
+switch (command) {
+  case "tree":
+    treeFn(inputArr[1]);
+    break;
+  case "organize":
+    organizeFn(inputArr[1]);
+    break;
+  case "help":
+    helpFn(inputArr[1]);
+    break;
+  default:
+    console.log("Please input right command");
+    break;
+}
+
+function treeFn(dirPath) {
+  console.log("Tree command implemented for ", dirPath);
+}
+
+function organizeFn(dirPath) {
+  //1. input -> directory path given
+  //2. create -> organized_files -> directory
+  //3. check all files and identify categories of all the files present in that input directorie
+  //4. copy / cut files to that organized directory
+  let destPath;
+  if (dirPath == undefined) {
+    console.log("Kindly enter the path");
+    return;
+  } else {
+    let doesExist = fs.existsSync(dirPath);
+    //fs.existsSync() -> returns boolean value, true if dirPath exists otherwise false.
+
+    if (doesExist) {
+      //2
+      destPath = path.join(dirPath, "organized_files");
+      if (fs.existsSync(destPath) == false) {
+        fs.mkdirSync(destPath);
+      }
+    } else {
+      console.log("Kindly enter the correct path");
+      //Incase the path doesn't exist
+    }
+  }
+  organizeHelper(dirPath, destPath);
+}
+
+function organizeHelper(src, dest) {
+  //3
+  let childNames = fs.readdirSync(src);
+  //console.log(childNames);
+  for (let i = 0; i < childNames.length; i++) {
+    let childAddress = path.join(src, childNames[i]);
+    let isFile = fs.lstatSync(childAddress).isFile();
+    if (isFile) {
+      //console.log(childNames[i]);
+      //4
+      let category = getCategory(childNames[i]);
+      console.log(childNames[i] + " belongs to ->> " + category);
+    }
+  }
+}
+
+function helpFn(dirPath) {
+  console.log(`
+  List of all commands:
+                  node main.js tree "directoryPath"
+                  node main.js organize "directoryPath"
+                  node main.js help
+  `);
+  //We have used template string using backticks otherwise we can put strings in single line only
+}
+
+function getCategory(name) {
+  let ext = path.extname(name);
+  ext = ext.slice(1);
+
+  for (let type in types) {
+    let cTypeArray = types[type];
+
+    for (let i = 0; i < cTypeArray.length; i++) {
+      if (ext == cTypeArray[i]) {
+        return type;
+      }
+    }
+  }
+
+  return "others";
+}
---
+++ utility.js
@@ -0,0 +1,21 @@
+let utility = {};
+utility.types = {
+  media: ["mp4", "mkv", "jpg", "jpeg"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "ods",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+  ],
+  app: ["exe", "dmg", "pkg", "deb"],
+};
+module.exports = utility;
